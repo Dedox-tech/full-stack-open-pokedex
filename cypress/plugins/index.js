524de7ad07ec090ff1dfda0bd4def1038bb98ab6
Diego Murillo <diegomurilloferrer@gmail.com>
2022-06-29 00:13:46 -0500

    Change filenames

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
